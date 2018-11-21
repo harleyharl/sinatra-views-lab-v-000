9b02503a1c3a23c013c380006d251d3f858d1fae
Harley Rayner <harleymteden@gmail.com>
2018-11-21 21:08:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ class App < Sinatra::Base
 		erb :hello
 	end
 
-	get '/goodbye' do 
-		erb :goodbye 
-	end 
+	get '/goodbye' do
+		erb :goodbye
+	end
 
-	get '/date' do 
+	get '/date' do
 		erb :date
 	end 
 
